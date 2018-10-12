09384021cdd4be7c4a6dd2e25bdd92d140450536
Quentin Hibon <quentin@chainsecurity.com>
2018-10-12 14:46:03 +0200

    use class-level singleton

--- src/main/java/ch/securify/SolidityResult.java
+++ src/main/java/ch/securify/SolidityResult.java
@@ -5,7 +5,14 @@ import ch.securify.analysis.SecurifyErrors;
 import java.util.HashMap;
 
 public class SolidityResult {
+    private static final SolidityResult INSTANCE = new SolidityResult();
+    private SolidityResult(){}
+
+    public static SolidityResult getInstance(){
+        return INSTANCE;
+    }
+
     HashMap<String, SmallPatternResult> results = new HashMap<>();
 
-    SecurifyErrors securifyErrors = null;
+    SecurifyErrors securifyErrors = new SecurifyErrors();
 }
